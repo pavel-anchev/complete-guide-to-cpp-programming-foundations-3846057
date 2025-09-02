a9a1af73c489fd2a31bc5e43c2a46513fe0aa8bf
Pavel <masive79@gmail.com>
2025-09-02 23:32:22 +0000

    Hello world with input

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,15 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char name[20];
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Hello, " << name << endl << flush;
+    return 0;
+}
